4dcae75d7cbb569d23bd556b6de9193194adab28
popagabrielliviu <168010715+popagabrielliviu@users.noreply.github.com>
2024-05-07 22:18:18 +0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,6 +1,10 @@
+function tellFortune(numarCopii, numePartener, locatieGeografica, locMunca){
+    let message = `Vei fi un ${locMunca} in ${locatieGeografica} ,casatorit cu ${numePartener} si vei avea ${numarCopii} copii.`
+    console.log (message)
+}
+
 let numarCopii = 3 
 let numePartener = "Emanuel"
 let locatieGeografica = "Italia"
 let locMunca = "Programator"
-let message = `Vei fi un ${locMunca} in ${locatieGeografica} ,casatorit cu ${numePartener} si vei avea ${numarCopii} copii.`
-console.log (message)
+tellFortune (numarCopii, numePartener, locatieGeografica, locMunca)
