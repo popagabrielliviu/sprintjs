f716bc4dca51b3ccb64a7d0225b68015ab3cf8f4
gabriel <liviu.gg@gmail.com>
2024-05-07 18:46:49 +0300

    tema 1

---
+++ index.js
@@ -0,0 +1,4 @@
+let firstNumber = 100
+let secondNumber = 10
+let divizibilitate = firstNumber / secondNumber === 10
+console.log(divizibilitate)
---
+++ index2.js
@@ -0,0 +1,6 @@
+let numarCopii = 3 
+let numePartener = "Emanuel"
+let locatieGeografica = "Italia"
+let locMunca = "Programator"
+let message = `Vei fi un ${locMunca} in ${locatieGeografica} ,casatorit cu ${numePartener} si vei avea ${numarCopii} copii.`
+console.log (message)
